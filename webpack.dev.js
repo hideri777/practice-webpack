656a035b5e0fc0544b354a37f3bbf4128edea06a
hideri <hori.hide1003@gmail.com>
2021-04-10 11:25:45 +0900

    webpack-dev-serverの導入

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,9 +1,15 @@
 const { merge } = require("webpack-merge");
 const commonConfig = require("./webpack.common");
+const path = require("path");
 
 // common環境と開発環境をmergeしている
 module.exports = merge(commonConfig, {
   mode: "development",
   watch: true,
   devtool: "cheap-module-eval-source-map", // ソースマップの生成をする エラー発生箇所のバンドル前のファイルが見れる
+  devServer: {
+    open: true, // サーバー起動時にブラウザが自動で開く
+    port: 9000,
+    contentBase: path.resolve(__dirname, "public"), // ルートディレクトリ
+  },
 });
