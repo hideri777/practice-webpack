6fc2773747046ac014ed73c7ee64cae7ec91ecc2
hideri <hori.hide1003@gmail.com>
2021-04-10 09:53:24 +0900

    webpackを導入してみる

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  // エントリーポイント
+  entry: "./src/js/app.js",
+  output: {
+    // 出力先 絶対パス
+    // osの依存なしで行けるようにpath.resolve利用
+    path: path.resolve(__dirname, "public"),
+    // 出力ファイル
+    filename: "js/bundle.js",
+  },
+};
