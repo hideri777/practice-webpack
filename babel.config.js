8d6cbaf74cb1ca35ca43980b54be4d68bb45e854
hideri <hori.hide1003@gmail.com>
2021-04-14 09:26:08 +0900

    ポリフィルの取り込み

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  presets: ["@babel/preset-env"],
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        useBuiltIns: "usage", // 必要なポリフィルのみ取り込み
+        corejs: 3, // 利用するcorejsのヴァージョン指定 デフォルトだと2だけどエラーが出るので3を指定する
+        // debug: true,
+      },
+    ],
+  ],
 };
--- src/js/app.js
+++ src/js/app.js
@@ -17,3 +17,14 @@ velocity($("h1"), "fadeIn", { duration: 2000, loop: true });
 
 const z = { z: 3 };
 console.log({ x: 1, y: 2, ...z }); // => {x:1, y:2, z:3}
+
+const promise = new Promise((resolve) => {
+  setTimeout(() => resolve("hello"), 3000);
+});
+
+async function delayHello() {
+  const value = await promise;
+  console.log(value);
+}
+
+delayHello();
