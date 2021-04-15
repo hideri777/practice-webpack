bc50506b3f72f613729979e1ceb158be70e4fd68
hideri <hori.hide1003@gmail.com>
2021-04-15 21:09:33 +0900

    prettierの追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
   parserOptions: {
     sourceType: "module", // import exportを利用してもエラーが発生しない
   },
-  extends: ["eslint:recommended"], // 外部のルールを指定する　eslintのおすすめを適用
+  extends: ["eslint:recommended", "plugin:prettier/recommended"], // 外部のルールを指定する　eslintのおすすめを適用
   rules: {
     "prefer-const": "error", // 更新しない変数宣言にconst利用してないとエラー
   },
